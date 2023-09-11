63a2cd848b0877afebe708921a5dd4c801e1ebc2
Namnpse <nam444479@gmail.com>
2023-09-11 23:01:31 +0700

    add fragments, colors, strings

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/MainActivity.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.namnp.modernfoodrecipeandroidapp
+package com.namnp.modernfoodrecipeandroidapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.namnp.modernfoodrecipeandroidapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
