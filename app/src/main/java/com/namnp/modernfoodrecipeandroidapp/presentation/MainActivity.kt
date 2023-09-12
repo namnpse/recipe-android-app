f266cac1c8eb052c50767a6d6312a2ebf1f8c993
Namnpse <nam444479@gmail.com>
2023-09-12 16:29:45 +0700

    set up food recipe navigation, add icon, setup bottom navigation, app bar with NavController

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/MainActivity.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/MainActivity.kt
@@ -2,11 +2,35 @@ package com.namnp.modernfoodrecipeandroidapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.namnp.modernfoodrecipeandroidapp.R
+import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appbarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment,
+            )
+        )
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appbarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
