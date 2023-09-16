8de592fd89f8eb7efc04a3caaa159edb2ac6f9b8
Namnpse <nam444479@gmail.com>
2023-09-16 12:39:55 +0700

    build shimmer item layout for recipe

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/RecipesFragment.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/RecipesFragment.kt
@@ -7,14 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.namnp.modernfoodrecipeandroidapp.R
 
-class RecipesFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
-    }
+class RecipesFragment : Fragment(R.layout.fragment_recipes) {
 
 }
