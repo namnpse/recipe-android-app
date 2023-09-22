2cc777dde1d01fe728d1eab78c956e009a5d8863
Namnpse <nam444479@gmail.com>
2023-09-22 18:16:09 +0700

    setup recipe adapter using diff util, data binding for recipe item layout

---
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/recipe/RecipesAdapter.kt
@@ -0,0 +1,53 @@
+package com.namnp.modernfoodrecipeandroidapp.presentation.features.recipe
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.namnp.modernfoodrecipeandroidapp.data.models.FoodRecipe
+import com.namnp.modernfoodrecipeandroidapp.data.models.Result
+import com.namnp.modernfoodrecipeandroidapp.databinding.RecipeItemLayoutBinding
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipes = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipeItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result){
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipeItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setData(newData: FoodRecipe){
+        val recipesDiffUtil =
+            RecipesDiffUtil(recipes, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
