87a16fc98720096f9247f59b25ae986c4b070708
Namnpse <nam444479@gmail.com>
2023-10-02 23:03:33 +0700

    add data store repository

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/recipe/RecipesViewModel.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/recipe/RecipesViewModel.kt
@@ -3,6 +3,9 @@ package com.namnp.modernfoodrecipeandroidapp.presentation.features.recipe
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.API_KEY
+import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.DEFAULT_DIET_TYPE
+import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_API_KEY
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_DIET
@@ -15,10 +18,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
     fun getQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERY_NUMBER] = "20"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = "snack"
-        queries[QUERY_DIET] = "vegan"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
