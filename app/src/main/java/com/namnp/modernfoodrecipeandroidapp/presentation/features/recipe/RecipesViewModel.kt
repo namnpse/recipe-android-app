b4716ade87be38a571c2c398807bf39bfa2f7446
Namnpse <nam444479@gmail.com>
2023-10-03 15:40:58 +0700

    handle logic in recipe view model

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/recipe/RecipesViewModel.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/recipe/RecipesViewModel.kt
@@ -1,7 +1,9 @@
 package com.namnp.modernfoodrecipeandroidapp.presentation.features.recipe
 
 import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.API_KEY
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.DEFAULT_DIET_TYPE
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -12,16 +14,40 @@ import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_D
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_NUMBER
 import com.namnp.modernfoodrecipeandroidapp.constant.Constants.Companion.QUERY_TYPE
+import com.namnp.modernfoodrecipeandroidapp.data.DataStoreRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+class RecipesViewModel @ViewModelInject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
     fun getQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
