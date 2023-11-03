611bdf73ac88773da6ce847454c526e58f4f5275
Namnpse <nam444479@gmail.com>
2023-11-03 22:30:03 +0700

    handle favorite recipe transactions in recipe dao

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/data/local/RecipesDao.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/data/local/RecipesDao.kt
@@ -1,9 +1,6 @@
 package com.namnp.modernfoodrecipeandroidapp.data.local
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,5 +11,17 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun getRecipes(): Flow<List<RecipesEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun getFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 
 }
