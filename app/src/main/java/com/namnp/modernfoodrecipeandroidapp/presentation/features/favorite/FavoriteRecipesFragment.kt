fe0636683464a1b84d37a2f4bab88b941924aec9
Namnpse <nam444479@gmail.com>
2023-11-10 07:15:11 +0700

    display favorite recipe data, bind data to favorite_recipe_item.xml

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/favorite/FavoriteRecipesFragment.kt
@@ -5,16 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.namnp.modernfoodrecipeandroidapp.R
+import com.namnp.modernfoodrecipeandroidapp.presentation.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.view.*
 
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private val favoriteRecipesAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        setupRecyclerView(view.favoriteRecipesRecyclerView)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainViewModel.localFavoriteRecipes.observe(viewLifecycleOwner, { favoritesEntity ->
+            favoriteRecipesAdapter.updateData(favoritesEntity)
+        })
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = favoriteRecipesAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
