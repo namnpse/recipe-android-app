d137f52d17e54a1a95b604ddfe32b5c634738e71
Namnpse <nam444479@gmail.com>
2023-12-19 19:03:59 +0900

    update data binding in favorite recipe fragment

--- app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/favorite/FavoriteRecipesBinding.kt
+++ app/src/main/java/com/namnp/modernfoodrecipeandroidapp/presentation/features/favorite/FavoriteRecipesBinding.kt
@@ -3,6 +3,8 @@ package com.namnp.modernfoodrecipeandroidapp.presentation.features.favorite
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.namnp.modernfoodrecipeandroidapp.data.local.FavoritesEntity
@@ -23,9 +25,11 @@ class FavoriteRecipesBinding {
                     is ImageView -> {
                         view.visibility = View.VISIBLE
                     }
+
                     is TextView -> {
                         view.visibility = View.VISIBLE
                     }
+
                     is RecyclerView -> {
                         view.visibility = View.INVISIBLE
                     }
@@ -35,9 +39,11 @@ class FavoriteRecipesBinding {
                     is ImageView -> {
                         view.visibility = View.INVISIBLE
                     }
+
                     is TextView -> {
                         view.visibility = View.INVISIBLE
                     }
+
                     is RecyclerView -> {
                         view.visibility = View.VISIBLE
                         adapter?.updateData(favoritesEntity)
@@ -45,6 +51,27 @@ class FavoriteRecipesBinding {
                 }
             }
         }
+
+        @BindingAdapter("data", "setAdapter", requireAll = false)
+        @JvmStatic
+        fun setVisibility(
+            view: View,
+            favoritesEntity: List<FavoritesEntity>?,
+            adapter: FavoriteRecipesAdapter?,
+        ) {
+            when (view) {
+                is RecyclerView -> {
+                    val hasNoData = favoritesEntity.isNullOrEmpty()
+                    view.isInvisible = hasNoData
+                    if (!hasNoData) {
+                        favoritesEntity?.let { adapter?.updateData(it) }
+                    }
+                }
+
+                else -> view.isVisible = favoritesEntity.isNullOrEmpty()
+            }
+        }
+
 
     }
 
